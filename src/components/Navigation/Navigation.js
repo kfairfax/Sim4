a2048d248d216e064a6c8b8f7e5b35105a6edf3c
Katrina Fairfax <fairfaxkatrina@gmail.com>
2018-07-06 16:19:04 -0600

    nav has links

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -1,11 +1,17 @@
 import React from 'react';
+import {Link} from 'react-router-dom';
 
 const Navigation = () => {
 
-    return(
-        <div>NavBar
-            <button>Cart</button>
-            <button>Home</button>
+    return (
+        <div>
+            <Link to = '/cart'>
+                <button>Cart</button>
+            </Link>
+
+            <Link to = '/'>
+                <button>Home</button>
+            </Link>
         </div>
     )
 };
